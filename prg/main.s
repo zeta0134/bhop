d07cc2b0cc805ba0e4c4614e5065b52a36a8aeaf
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-07-14 19:01:24 -0400

    Add some additional bhops for testing

--- prg/main.s
+++ prg/main.s
@@ -15,7 +15,9 @@ nmi_counter: .byte $00
         .segment "PRG1_A000"
         ;.include "../ftm/yakra.asm"
         ;.include "../ftm/sanctuary.asm"
-        .include "../ftm/bark.asm"
+        ;.include "../ftm/bark.asm"
+        ;.include "../ftm/bah.asm"
+        .include "../ftm/1-1.asm"
 
         .segment "PRG3_E000"
         .export start, nmi, irq
