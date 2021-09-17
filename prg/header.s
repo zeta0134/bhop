2e8a48d6c78947bf2546c578ccf7fc787382eace
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-09-17 16:42:36 -0400

    Set the project up for obscene amounts of SONG/DPCM banks

--- prg/header.s
+++ prg/header.s
@@ -5,7 +5,7 @@
 ;
 .segment "HEADER"
         .byte "NES", $1a
-        .byte $08               ; 4x 16KB PRG-ROM banks = 64 KB total
+        .byte $10               ; 16x 16KB PRG-ROM banks = 256 KB total
         .byte $01               ; 1x 8KB CHR-ROM banks = 8 KB total
         .byte $40, $00          ; MMC3 without battery-backed PRG RAM
         .byte $00               ; No PRG RAM (ines 1.0 will give us 8k, which we will ignore)
