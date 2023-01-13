7705a7b37d663384c402ad15ede1ca02a3e641f2
Nicholas Flynt <thegamefreak0134@gmail.com>
2023-01-13 07:34:28 -0500

    Get bhop interactions all moved into the new player module

--- demos/common/player.s
+++ demos/common/player.s
@@ -1,9 +1,54 @@
         .setcpu "6502"
         .include "nes.inc"
+        .include "player.inc"
         .include "../../bhop/bhop.inc"
 
         .zeropage
+CurrentTrack: .res 1
+TrackPtr: .res 2
 
         .segment "CODE"
 
+; External Functions, declared in player.inc
 
+.proc player_init
+        lda #0
+        sta CurrentTrack
+        jsr initialize_current_track
+        rts
+.endproc
+
+.proc player_update
+        jsr bhop_play
+        rts
+.endproc
+
+; Banking, needed by bhop. This is a thin wrapper around the player-specific
+; banking function
+
+.proc bhop_apply_dpcm_bank
+        jsr player_bank_samples
+        rts
+.endproc
+.export bhop_apply_dpcm_bank
+
+; Internal Functions
+
+.proc initialize_current_track
+        lda CurrentTrack
+        asl
+        tax
+        lda music_track_table+0, x
+        sta TrackPtr+0
+        lda music_track_table+1, x
+        sta TrackPtr+1
+
+        ldy MusicTrack::BankNumber
+        lda (TrackPtr), y
+        jsr player_bank_music
+
+        ldy MusicTrack::BankNumber
+        lda (TrackPtr), y
+        jsr bhop_init
+        rts
+.endproc
--- demos/zsaw/prg/main.s
+++ demos/zsaw/prg/main.s
@@ -6,7 +6,6 @@
         .include "../../common/ppu.inc"
         .include "../../common/word_util.inc"
 
-        .include "../../../bhop/bhop.inc"
         .include "../../../bhop/zsaw.inc"
 
         .zeropage
@@ -15,10 +14,30 @@
 nmi_counter: .byte $00
 
         .segment "PRG0_8000"
-        .export start, bhop_nmi, bhop_music_data
+        .export start, demo_nmi, bhop_music_data
 
 bhop_music_data:
-        .include "../music/zsaw_tactus.asm"
+        .include "../music/zsaw_demo_tracks.asm"
+
+;                   Bank  Track#                         Title                        Artist
+;                    ---     ---  ----------------------------  ----------------------------
+song_tactus:     
+        music_track    0,      0,              "Tactus - Demo",                   "zeta0134"
+song_heat_death: 
+        music_track    0,      1,        "Heat Death - Smooth",                   "zeta0134"
+
+music_track_table:
+        .addr song_tactus
+        .addr song_heat_death
+
+; NROM doesn't support banking at all, so stub both of these out
+.proc player_bank_music
+        rts
+.endproc
+
+.proc player_bank_samples
+        rts
+.endproc
 
 .proc wait_for_nmi
         lda nmi_counter
@@ -42,9 +61,8 @@ loop:
         ; z-saw init
         jsr zsaw_init
 
-        ; bhop init
-        lda #0 ; song index
-        jsr bhop_init
+        ; player init
+        jsr player_init
 
         ; re-enable graphics and NMI
         lda #$1E
@@ -52,18 +70,18 @@ loop:
         lda #(VBLANK_NMI | OBJ_0000 | BG_0000)
         sta PPUCTRL
 
-        ; setup for measuring performance
+        ; todo: setup for measuring performance?
         jsr wait_for_nmi ; safety sync
 
 gameloop:
         jsr poll_input
-        jsr bhop_play
+        jsr player_update
         jsr wait_for_nmi ; safety sync
         jmp gameloop ; forever
 
 .endproc
 
-.proc bhop_nmi
+.proc demo_nmi
         ; preserve registers
         pha
         txa
