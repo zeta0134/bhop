a8c05707f1935301273c9f0fbc4a5b8bdb97558d
Nicholas Flynt <thegamefreak0134@gmail.com>
2023-12-28 22:00:53 -0600

    Get VRC6 pulse/sawtooth table lookup implemented and working

--- bhop/util.asm
+++ bhop/util.asm
@@ -3,41 +3,60 @@
 ; All three will clobber Y, and differ only in their output
 ; The channel index (X) determines which pitch table is used
 
-.proc set_channel_base_frequency
-    tay
-    
-    ; TODO: other channel pitch table logic here
+.macro __set_frequency target_low, target_high
+.scope
+    tay ; Y will index into the chosen pitch table
 
+    ; some expansion chips have separate initial pitch tables, check for
+    ; and use those here
+.if ::BHOP_VRC6_ENABLED
+    cpx #VRC6_SAWTOOTH_INDEX
+    bne not_saw
+    ; do saw here
+    lda vrc6_sawtooth_period_low, y
+    sta target_low
+    lda vrc6_sawtooth_period_high, y
+    sta target_high
+    rts
+
+not_saw:
+    cpx #VRC6_PULSE_1_INDEX   
+    bne not_vrc6_pulse_1
+    jmp is_vrc6_pulse
+not_vrc6_pulse_1:
+    cpx #VRC6_PULSE_2_INDEX   
+    bne not_vrc6
+is_vrc6_pulse:
+    ; do vrc6 pulse here
+    lda vrc6_pulse_period_low, y
+    sta target_low
+    lda vrc6_pulse_period_high, y
+    sta target_high
+    rts
+
+not_vrc6:
+.endif
+    
+    ; if we don't hit a special case, fall through to typical 2A03
 ntsc_2a03:
     lda ntsc_period_low, y
-    sta channel_base_frequency_low, x
+    sta target_low
     lda ntsc_period_high, y
-    sta channel_base_frequency_high, x
+    sta target_high
+.endscope
+.endmacro
+
+.proc set_channel_base_frequency
+    __set_frequency {channel_base_frequency_low, x}, {channel_base_frequency_high, x}
     rts
 .endproc
 
 .proc set_channel_relative_frequency
-    tay
-    
-    ; TODO: other channel pitch table logic here
-
-ntsc_2a03:
-    lda ntsc_period_low, y
-    sta channel_relative_frequency_low, x
-    lda ntsc_period_high, y
-    sta channel_relative_frequency_high, x
+    __set_frequency {channel_relative_frequency_low, x}, {channel_relative_frequency_high, x}
     rts
 .endproc
 
 .proc set_scratch_target_frequency
-    tay
-    
-    ; TODO: other channel pitch table logic here
-
-ntsc_2a03:
-    lda ntsc_period_low, y
-    sta scratch_target_frequency
-    lda ntsc_period_high, y
-    sta scratch_target_frequency+1
+    __set_frequency {scratch_target_frequency}, {scratch_target_frequency+1}
     rts
 .endproc
